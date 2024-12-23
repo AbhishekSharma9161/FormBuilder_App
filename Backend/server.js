8d0df5a2e3c27e4d0212cd5edf7593a6429a9f3d
Code-Abhi <93138322+AbhishekSharma9161@users.noreply.github.com>
2024-12-23 21:11:02 +0530

    commit

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+const cors  = require("cors");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/userRoutes");
+dotenv.config();
+
+
+app.use(cors());
+app.use(bodyParser.json()); //it parse the json data
+app.use(bodyParser.urlencoded({extended:true})); //it also parse the form data
+app.use("api/user",userRoute);
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=>console.log("MongoDB Connected Successfully!!"))
+.catch((err)=>console.log("MongoDB Connection Error: ",err));
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to FormBot App Backend !!");
+})
+
+const PORT  = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
